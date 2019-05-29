e65d86313bc664c2ce9c11c0f0d3279ab3b2404b
IzumixModZ <51163724+IzumixModZ@users.noreply.github.com>
2019-05-29 23:16:20 +0900

    Add files via upload

---
+++ CHEAT SAMPLE FOR CSharp/Form1.Designer.cs
@@ -0,0 +1,116 @@
+﻿namespace CHEAT_SAMPLE_FOR_CSharp
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// 必要なデザイナー変数です。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 使用中のリソースをすべてクリーンアップします。
+        /// </summary>
+        /// <param name="disposing">マネージド リソースを破棄する場合は true を指定し、その他の場合は false を指定します。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows フォーム デザイナーで生成されたコード
+
+        /// <summary>
+        /// デザイナー サポートに必要なメソッドです。このメソッドの内容を
+        /// コード エディターで変更しないでください。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.checkBox1 = new System.Windows.Forms.CheckBox();
+            this.button2 = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(13, 13);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(352, 23);
+            this.textBox1.TabIndex = 0;
+            this.textBox1.Text = "Your Wii U IP Address";
+            this.textBox1.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(371, 13);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(160, 23);
+            this.button1.TabIndex = 1;
+            this.button1.Text = "Connect to Wii U";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // checkBox1
+            // 
+            this.checkBox1.AutoSize = true;
+            this.checkBox1.Location = new System.Drawing.Point(13, 42);
+            this.checkBox1.Name = "checkBox1";
+            this.checkBox1.Size = new System.Drawing.Size(119, 19);
+            this.checkBox1.TabIndex = 2;
+            this.checkBox1.Text = "Sample Cheat 1";
+            this.checkBox1.UseVisualStyleBackColor = true;
+            this.checkBox1.CheckedChanged += new System.EventHandler(this.checkBox1_CheckedChanged);
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(13, 67);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(119, 23);
+            this.button2.TabIndex = 3;
+            this.button2.Text = "Sample Cheat 2";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(12, 323);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(176, 15);
+            this.label1.TabIndex = 4;
+            this.label1.Text = "Copyright 🄫 2019 IzumiPlaza";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(543, 347);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.checkBox1);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.textBox1);
+            this.Font = new System.Drawing.Font("Meiryo UI", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(128)));
+            this.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
+            this.Name = "Form1";
+            this.Text = "Cheat Sample CSharp";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.CheckBox checkBox1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Label label1;
+    }
+}
+
---
+++ CHEAT SAMPLE FOR CSharp/Form1.cs
@@ -0,0 +1,83 @@
+﻿//Hi, My Name is Izumi.
+//This Program is Cheat Code Sender Assist Source
+//Framework = 4.6.1.
+//Contact Discord : Izumi_2147483647#9849
+//I play Minecraft, MarioKart 8, Splatoon.
+//Enjoy Cheat!!
+
+//Copyright 🄫 2019 IzumiPlaza
+
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace CHEAT_SAMPLE_FOR_CSharp
+{
+    public partial class Form1 : Form
+    {
+        public TCPGecko GckApi;
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void checkBox1_CheckedChanged(object sender, EventArgs e)
+        {
+            //Sample / Example Code Normal Code(Not Pointer);
+            //GckApi.byteary_poke(0x10000000, new byte[] { 0x10, 0x20, 0x30, 0x40 } );
+            //This Command is 0x10000000 Write 0x10203040;
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            //Sample / Example Code Pointer Code;
+            //Test ptr = GckApi.peek(0x10000000);
+            //ptr += 0x10;
+            //GckApi.byteary_poke(ptr, new byte[] { 0x10, 0x20, 0x30, 0x40 } );
+
+            //and Pointer in Pointer;
+
+            //Sample / Example Code Pointer Code;
+            //Test ptr2 = GckApi.peek(GckApi.peek(0x10000000) + 0x10);
+            //ptr2 += 0x10;
+            //GckApi.byteary_poke(ptr2, new byte[] { 0x10, 0x20, 0x30, 0x40 } );
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            //if(button1.Text == "Connect to Wii U")
+            //{
+                //bool GckConnect = true;
+                //GckApi = new TCPGecko(textBox1.Text, 7331);
+                //GckConnect = GckApi.Connect();
+                //if(GckConnect)
+                //{
+                    //button1.Text = "Disconnect Wii U";
+                    //button2.Enabled = true;
+                    //checkBox1.Enabled = true;
+                //}
+            //}
+            //else
+            //{
+                //GckApi.Disconnect();
+                //button1.Text = "Connect to Wii U";
+                //button2.Enabled = false;
+                //checkBox1.Enabled = false;
+            //}
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            //button2.Enabled = false;
+            //checkBox1.Enabled = false;
+        }
+    }
+}
+
+//Cheat Sample for C# by _Izumi_
